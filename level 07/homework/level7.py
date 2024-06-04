7f52c2e3c355263ddff82b8a73086c68ad713d16
bochoidze <kharazishvili1983@gmail.com>
2024-06-04 21:41:06 +0400

    homework

---
+++ level 07/homework/level7.py
@@ -0,0 +1,322 @@
+# 1 დავალება
+#სახელი 
+name = "luka"
+
+#გვარი
+
+sir_name = "bochoidze"
+#ასაკი
+age = 13
+#ქვეყანა
+country = "georgia"
+#საყვარელი მანქანა
+favorite_car = " Chevrolet Camaro"
+#საყვარელი ფერი 
+favorite_color = "blue"
+#საყვარელი რიცხვი
+favorite_number = 10
+
+name = input(" what is your name: ")
+
+sir_name = input("what is your last name: ")
+
+age = input("what is your age: ")
+
+
+country = input("where are you from: ")
+
+
+favorite_car  = input("What is your favorite car: ")
+
+
+favorite_color = input("what is your favorite color: ")
+
+
+favorite_number = input("What is your favorite number: ")
+
+print ("my name is",name,"My last name is",sir_name,"my age is",age,"I am",country,"my favorite car is",favorite_car,
+"my favorite color is",favorite_color,"My favorite number is",favorite_number) 
+# 3 დავალება
+# 1 ციფრი 10
+# მიმატება
+print (10+20)
+
+
+print (10+1)
+print (10+2)
+print (10+16)
+print (10+14)
+print (10+7)
+print (10+80)
+print (10+34)
+print (10+32)
+print (10+9)
+print (10+10)
+print (10+11)
+print (10+81)
+print (10+100)
+print (10+13)
+print (10+19)
+
+# გამოკლება
+print (10-20)
+print (10-1)
+print (10-2)
+print (10-16)
+print (10-14)
+print (10-7)
+print (10-80)
+print (10-34)
+print (10-32)
+print (10-9)
+print (10-10)
+print (10-11)
+print (10-81)
+print (10-100)
+print (10-13)
+print (10-19)
+print (10-20)
+
+# გამრავლება
+print (10*1)
+print (10*2)
+print (10*16)
+print (10*14)
+print(10*80)
+print (10*34)
+print (10*32)
+print (10*9)
+print (10*10)
+print (10*11)
+print (10*81)
+print (10*100)
+print (10*13)
+print (10*19)
+print (10*20)
+#გაყოფა
+print (10//1)
+print (10//2)
+print (10//3)
+print (10//1.9)
+print (10//9.2)
+print (10//9)
+print (10*10)
+
+
+
+
+
+print (10//4)
+print(10//5)
+print (10//5.9)
+print (10//6)
+print (10//8.2)
+print (10//9.4)
+print (10//8)
+print (10//4.8)
+
+# 2-რე ციფრი 100
+
+# მიმატება
+print (100+1)
+print (100+2)
+print (100+16)
+print (100+14)
+print (100+7)
+print (100+80)
+print (100+34)
+print (100+32)
+print (100+9)
+print (100+10)
+print (100+11)
+print (100+81)
+print (100+100)
+print (100+13)
+print (100+19)
+# გამოკლება
+
+print (100-20)
+print (100-1)
+print (100-2)
+print (100-16)
+print (100-14)
+print (100-7)
+print (100-80)
+print (100-34)
+print (100-32)
+print (100-9)
+print (100-10)
+print (100-11)
+print (100-81)
+print (100-100)
+print (100-13)
+print (100-19)
+print (100-20)
+
+#გამრავლება
+print (100*1)
+print (100*2)
+print (100*16)
+print (100*14)
+print(100*80)
+print (100*34)
+print (100*32)
+print (100*9)
+print (100*10)
+print (100*11)
+print (100*81)
+print (100*100)
+print (100*13)
+print (100*19)
+print (100*20)
+# გაყოფა
+print (100//1)
+print (100//2)
+print (100//3)
+print (100//1.9)
+print (100//9.2)
+print (100//9)
+print (100*10)
+
+
+
+
+
+print (100//4)
+print(100//5)
+print (100//5.9)
+print (100//6)
+print (100//8.2)
+print (100//9.4)
+print (100//8)
+print (100//4.8)
+
+
+
+# 3-მე ციპრი 1500
+# მიმატება
+print (1500+1)
+print (1500+2)
+print (1500+16)
+print (1500+14)
+print (1500+7)
+print (1500+80)
+print (1500+34)
+print (1500+32)
+print (1500+9)
+print (1500+10)
+print (1500+11)
+print (1500+81)
+print (1500+100)
+print (1500+13)
+print (1500+19)
+# გამოკლება
+
+print (1500-20)
+print (1500-1)
+print (1500-2)
+print (1500-16)
+print (1500-14)
+print (1500-7)
+print (1500-80)
+print (1500-34)
+print (1500-32)
+print (1500-9)
+print (1500-10)
+print (1500-11)
+print (1500-81)
+print (1500-100)
+print (1500-13)
+print (1500-19)
+print (1500-20)
+
+#გამრავლება
+print (1500*1)
+print (1500*2)
+print (1500*16)
+print (1500*14)
+print(1500*80)
+print (1500*34)
+print (1500*32)
+print (1500*9)
+print (1500*10)
+print (1500*11)
+print (1500*81)
+print (1500*100)
+print (1500*13)
+print (1500*19)
+print (1500*20)
+# გაყოფა
+print (1500//1)
+print (1500//2)
+print (1500//3)
+print (1500//1.9)
+print (1500//9.2)
+print (1500//9)
+print (1500*10)
+
+
+
+
+
+print (1500//4)
+print(1500//5)
+print (1500//5.9)
+print (1500//6)
+print (1500//8.2)
+print (1500//9.4)
+print (1500//8)
+print (1500//4.8)
+
+
+
+
+
+# 2 დავალება
+name = "gio"
+name = "nika"
+
+sir_name = "shubitidze"
+
+sir_name = "gelasvili"
+
+age = 14
+age = 16
+
+country = "georgia"
+country = "georgia"
+
+
+
+
+
+
+print ("Hello")
+print ("Hello")
+name = input(" what is your name: ")
+print ("my name is",name)
+sir_name = input("and what is your last name: ")
+print ("My last name is",sir_name)
+age = input("and age: ")
+print ("my age is",age)
+country = input("where are you from: ")
+print ("I am",country)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
