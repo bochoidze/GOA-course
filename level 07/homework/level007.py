553e1c59c705aaf1596faae7aa4a30ff1115fb21
bochoidze <kharazishvili1983@gmail.com>
2024-06-05 23:07:14 +0400

    homework

---
+++ level 07/homework/level007.py
@@ -0,0 +1,30 @@
+#დავალება 2
+number100 = int(input("100:"))
+number125 = int(input("125:"))
+number150= int(input("150:"))
+print(number100 + number125)
+print(number100 + number125+number150)
+print(number100 * number125//number150)
+print(number100 + number125//number150)
+print(number100 // number125*number150)
+print(number150 + number125-number100)
+print(number100 + number150)
+print(number100 + number125-number150)
+print(number100 * number125-number150)
+print(number100 // number125-number150)
+print(number125 - number100*number150)
+print(number150 // number100*number125)
+print(number150 + number100+number125)
+print(number150 * number100*number125)
+print(number150 + number100*number125)
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 07/homework/level07.py
@@ -0,0 +1,31 @@
+# 3 დავალება
+name = "gio"
+name = "nika"
+
+sir_name = "shubitidze"
+
+sir_name = "gelasvili"
+
+age = 14
+age = 16
+
+country = "georgia"
+
+
+
+
+
+
+
+
+print ("Hello")
+print ("Hello")
+
+name = input(" what is your name: ")
+print ("my name is",name)
+sir_name = input("and what is your last name: ")
+print ("My last name is",sir_name)
+age = input("and age: ")
+print ("my age is",age)
+country = input("where are you from: ")
+print ("I am",country)
