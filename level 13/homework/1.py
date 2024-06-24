c5170885266897df79e704609a7bc30aa6ddd72d
bochoidze <kharazishvili1983@gmail.com>
2024-06-24 18:10:56 +0400

    homework

---
+++ level 13/homework/1.py
@@ -0,0 +1,18 @@
+print(10+40_9>10-6+8)
+a = 7
+b = 8
+print(a<b)
+print(123*123>40*40)
+print(35>7)
+aa = "True"
+bb = "False"
+print(90>89-20)
+a_b = "False"
+b_a = "True"
+print(a_b and b_a)
+print(9<10)
+print(456+7<892)
+print(125.5>125)
+print(7845>13*56)
+
+
