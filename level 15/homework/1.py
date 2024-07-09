6554671922bd3db1c9c59c8da4169f62a89ddac1
bochoidze <kharazishvili1983@gmail.com>
2024-07-09 21:39:07 +0400

    homework

---
+++ level 15/homework/1.py
@@ -0,0 +1,23 @@
+# 1 homework
+num = int(input('enter a num from 1 to 1000: '))
+
+guess_num = 500
+
+is_equal = num == guess_num
+
+print('your final answer is correct', is_equal)
+#2 homework
+item = int(input("enter items's that u liked: "))
+price = int(input("enter item's price"))
+
+
+print(item * price)
+# 3 homework
+weight = int(input('enter ur kg: '))
+
+height = float(input('enter ur cm: '))
+
+bmi_F = weight / (height * height)
+
+print(int(bmi_F))
+
---
+++ level 15/homework/3.py
@@ -0,0 +1,23 @@
+type_0 = "football"
+type_1 =  4.9
+type_2 = 78
+type_3 = "78"
+type_4 = "89.0 t"
+type_5 = 56
+type_6 = 98.265
+type_7 = "GOA"
+type_8 = 90
+type_9 = "luka"
+
+print(type(type_0))
+print(type(type_1))
+print(type(type_2))
+print(type(type_3))
+print(type(type_4))
+print(type(type_5))
+print(type(type_6))
+print(type(type_7))
+print(type(type_8))
+print(type(type_9))
+
+
