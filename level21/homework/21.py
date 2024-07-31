467db650f782d28a8bd5da181bb335f5bcdc2462
bochoidze <kharazishvili1983@gmail.com>
2024-07-31 11:05:55 +0400

    homework

---
+++ level21/homework/21.py
@@ -0,0 +1,13 @@
+# 1 დაპრინტვა და შეკრება
+print("GOA")
+print(90+67-8)
+print(56)
+print("GOA best")
+print(90//23*45-90+7)
+print(34+7)
+# 2 მეტობა და ნაკლებობა
+print(78+5>9)
+print(90-45<50)
+print(9<10)
+print(56>23)
+print(67+ 68>56)
