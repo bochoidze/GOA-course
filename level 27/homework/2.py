426ced9a5250fa33626d2ba157a766dcb82c43cd
bochoidze <kharazishvili1983@gmail.com>
2024-08-13 09:06:55 +0400

    homework

---
+++ level 27/homework/2.py
@@ -0,0 +1,14 @@
+sia = ['s', 'b', 'gamarjoba', 'rogor xar', '!', '?', 'chad', 'zd', 'f', 'h']
+
+sia[2] = 'gagimarjos'
+
+sia[3] = 'kargad shen?'
+
+sia[8] = '145'
+
+sia[9] = 'sf'
+
+sia[6] = '?????'
+
+
+print(sia)
---
+++ level 27/homework/4.py
@@ -0,0 +1,13 @@
+sia = [10, 4, 6.8, 91, 23.6, 68]
+
+print(sia [0]* 2)
+
+print(sia [1]* 2)
+
+print(sia [2]* 2)
+
+print(sia [3]* 2)
+
+print(sia [4]* 2)
+
+print(sia [5]* 2)
