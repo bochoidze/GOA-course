0f6a6b663f3e1736ff01bb1d915c44a8820d1c39
bochoidze <kharazishvili1983@gmail.com>
2024-08-23 17:56:35 +0400

    homework

---
+++ mini lider work 1/homework/1.py
@@ -0,0 +1,19 @@
+print('your account  is lj23ut9604699365849302' )
+
+print(input('enter your account:'))
+
+print('you have 560 GEL on your account')
+
+
+
+
+
+ansver = int(input("how much money do you want to withdraw from the deposit: "))
+
+
+if ansver <= 560:
+    print("you withdraw money")
+
+else: 
+   print("you dont have enough money in your account try again leiter")
+   
