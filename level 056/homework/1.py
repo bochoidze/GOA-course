1e411dc08459987325e2723bcb8ec394748030ed
bochoidze <kharazisvili1983@gmail.com>
2025-05-10 13:56:54 +0400

    homework

---
+++ level 056/homework/1.py
@@ -0,0 +1,9 @@
+def array_plus_array(arr1,arr2):
+    res = 0
+    for i in  range (len(arr1)):
+        res +=arr1[i]
+
+         for i in  range (len(arr2)):
+
+            res +=arr2[j]
+    return res
---
+++ level 067/homework/1.py
@@ -0,0 +1,5 @@
+def between(a,b):
+    array = []
+    for i in range(a,b + 1):
+      array.append(1)
+    return array
